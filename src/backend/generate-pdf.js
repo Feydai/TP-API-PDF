d936ce72637b6a23711aab172c6e066200b5e163
lsun <lsun.github@gmail.com>
2024-02-01 16:32:59 +0100

    add: generate simple pdf

--- src/backend/generate-pdf.js
+++ src/backend/generate-pdf.js
@@ -1,15 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
+const express = require("express");
+const cors = require("cors");
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
 const port = 3000;
 const app = express();
+app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.get("/pdf", (req, res) => {
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream("output.pdf"));
+  doc.text("Hello, world!");
+  doc.end();
+  res.send("PDF generated!");
+});
+
+app.put("/pdf", (req, res) => {
+  try {
+    const doc = new PDFDocument();
+    const value = req.body.text;
+    doc.pipe(fs.createWriteStream("test.pdf"));
+    doc.text(value);
+    doc.end();
+    res.status(200).send("PDF generated!");
+  } catch (error) {
+    console.error("An error occurred while generating the PDF:", error);
+    res.status(500).send("Error generating PDF");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
